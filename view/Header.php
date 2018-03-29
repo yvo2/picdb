461e9f3ee83a21446c54b59ed0bc957448318c50
Lars <lars.baertschi@bluewin.ch>
2018-03-29 08:10:05 +0200

    + HTML Standard

--- view/Header.php
+++ view/Header.php
@@ -7,13 +7,13 @@ $sessionManager = new SessionManager();
 <html>
 <head>
     <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="/frontend/css/materialize.min.css"  media="screen"/>
-    <link type="text/css" rel="stylesheet" href="/frontend/css/style.css"  media="screen"/>
+    <link type="text/css" rel="stylesheet" href="/frontend/css/materialize.min.css"  media="screen" />
+    <link type="text/css" rel="stylesheet" href="/frontend/css/style.css"  media="screen" />
 
     <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <title>Blog</title>
 </head>
