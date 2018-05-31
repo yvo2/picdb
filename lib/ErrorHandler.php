abf475eb65f97beb6ccb0f863274bfce171f92cb
Lars <lars.baertschi@bluewin.ch>
2018-05-31 08:30:31 +0200

    + Fixed borken error handler

--- lib/ErrorHandler.php
+++ lib/ErrorHandler.php
@@ -5,18 +5,22 @@ set_error_handler("customError");
 function customError($errno, $errstr) {
     $viewCompatMode = true;
 
-    require_once "view/Header.php";
+    if (error_reporting() == 0) {
+        return;
+    }
 
     if (strpos($errstr, 'HY000/2002') !== false) {
         printError("Die Verbindung zu der Datenbank ist fehlgeschlagen. Bitte versuchen sie es später erneut.");
     } else {
+        // Debug
+        var_dump($errstr);
         printError("Unbekannter Fehler. Bitte Kontaktieren Sie die Betreiber oder versuchen sie es später erneut.");
-    }
-
-    require_once "view/Footer.php";
-    die();
+    };
 }
 
 function printError($msg) {
+    require_once "view/Header.php";
     echo "<div class='content'><h1>Systemfehler</h1><p>Es ist ein Fehler auf dem Server aufgetreten.</p><p>$msg</p></div>";
+    require_once "view/Footer.php";
+    die();
 }
